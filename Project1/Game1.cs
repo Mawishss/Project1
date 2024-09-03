40ec098b39a696c4b1d8355d7a69b545c966fe98
CAMT <CAMT@C207-3>
2024-09-03 16:30:28 +0700

    plAYERcONTROL

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -9,6 +9,8 @@ namespace Project1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        int x = 1;
+
         //update feature1
 
         public Game1()
